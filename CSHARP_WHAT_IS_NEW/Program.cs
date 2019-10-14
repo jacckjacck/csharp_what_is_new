3653944d9cb10c1cc0ce54a1fb4724d4026f4663
jorge gomez <jorge.gomez@tumesadeayuda.com>
2019-10-14 15:29:58 -0500

    first commit

---
+++ CSHARP_WHAT_IS_NEW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHARP_WHAT_IS_NEW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
